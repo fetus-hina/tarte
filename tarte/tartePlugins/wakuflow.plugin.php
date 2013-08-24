5a240c2150a1f9444cd9ac1590c8c265d4c44454
HiNa <hina@bouhime.com>
2013-08-25 00:17:45 +0900

    上下・左右反転

--- tarte/tartePlugins/wakuflow.plugin.php
+++ tarte/tartePlugins/wakuflow.plugin.php
@@ -35,7 +35,7 @@ function plugin_wakuflow(TwStatus $status = null, DictionaryCandidate $candidate
         unset($client);
 
         $commands = array();
-        if(!preg_match_all('/新枠|ろまのふ|モノクロ|白黒|セピア|(?:2|二)値化|(?:8|八)色|色反転|ネガ|ぼかし|シャープ|半額|中破|大破/u', $status->parsed->text, $matches, PREG_SET_ORDER)) {
+        if(!preg_match_all('/新枠|ろまのふ|モノクロ|白黒|セピア|(?:2|二)値化|(?:8|八)色|色反転|ネガ|ぼかし|シャープ|上下反転|左右反転|半額|中破|大破/u', $status->parsed->text, $matches, PREG_SET_ORDER)) {
             @unlink($tmp_out);
             @unlink($tmp_in);
             return '(わーくフローの解析に失敗)';
@@ -69,6 +69,12 @@ function plugin_wakuflow(TwStatus $status = null, DictionaryCandidate $candidate
                 break;
             case 'ぼかし':
                 $commands[] = 'gaussian_blur';
+                break;
+            case '上下反転':
+                $commands[] = 'flip vertical';
+                break;
+            case '左右反転':
+                $commands[] = 'flip horizontal';
                 break;
             case 'シャープ':
                 $commands[] = 'sharpen';
