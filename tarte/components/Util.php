476e9a937c7cd66964263ff67ace18d023c0608f
HiNa <hina@bouhime.com>
2014-03-31 14:30:21 +0900

    ユーザ名にURLっぽい文字列を含むとリンクされてしまう

--- tarte/components/Util.php
+++ tarte/components/Util.php
@@ -3,8 +3,8 @@ require_once(__DIR__ . '/../../../mb_str_replace/src/mb_str_replace.function.php
 class Util {
     static public function ignoreTwitterAutoLink($text) {
         return mb_str_replace(
-            array('@', '＠', '#', '＃'),
-            array('@ ', '＠ ', '# ', '＃ '),
+            array('@', '＠', '#', '＃', '://', '.'),
+            array('@ ', '＠ ', '# ', '＃ ', ' :// ', ' . '),
             $text,
             'UTF-8'
         );
