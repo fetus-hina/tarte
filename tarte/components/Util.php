6ea0556f1e10339b7ed94f954c56c3738bd67922
AIZAWA, Hina <hina@bouhime.com>
2014-05-16 02:24:50 +0900

    mb_str_replace の読み込みパスを修正

--- tarte/components/Util.php
+++ tarte/components/Util.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__ . '/../../../mb_str_replace/src/mb_str_replace.function.php');
+require_once(__DIR__ . '/../../__sub_modules/mb_str_replace/src/mb_str_replace.function.php');
 class Util {
     static public function ignoreTwitterAutoLink($text) {
         return mb_str_replace(
