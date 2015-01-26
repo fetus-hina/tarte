3059b86523d2948ff7abf7da4aa54bb67542979c
AIZAWA, Hina <hina@bouhime.com>
2015-01-26 20:12:36 +0900

    Yahoo(.com)の天気予報プログラムをchomado_botからぱくった

---
+++ tarte/components/YahooWeather.php
@@ -0,0 +1,152 @@
+<?php
+class YahooWeather {
+    public function get($location) {
+        $obj = $this->query($location);
+        $item = $obj->item;
+        $tomorrow = $item->forecast[1]; // fixme
+        $ret = [
+            'now' => [
+                'temp'      => self::formatTemperature($item->condition->temp, $obj->units->temperature),
+                'humidity'  => sprintf('%d%%', $obj->atmosphere->humidity),
+                'pressure'  => self::formatPressure($obj->atmosphere->pressure, $obj->units->pressure),
+                'wind'      => self::formatWind($obj->wind->direction, $obj->wind->speed, $obj->units->speed),
+                'weather'   => self::formatWeather($item->condition->code, $item->condition->text),
+            ],
+            'tomorrow' => [
+                'temp_l'    => self::formatTemperature($tomorrow->low, $obj->units->temperature),
+                'temp_h'    => self::formatTemperature($tomorrow->high, $obj->units->temperature),
+                'weather'   => self::formatWeather($tomorrow->code, $tomorrow->text),
+            ],
+            'updated_at'    => @strtotime($item->pubDate),
+        ];
+        return $ret;
+    }
+
+    private function query($location) {
+        $parameters = [
+            'q'         => sprintf('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="%s")', addslashes($location)),
+            'format'    => 'json',
+            'env'       => 'store://datatables.org/alltableswithkeys',
+        ];
+        return $this->requestJson(
+            'https://query.yahooapis.com/v1/public/yql?' . http_build_query($parameters, '', '&', PHP_QUERY_RFC3986)
+        );
+    }
+
+    private function requestJson($uri) {
+        $curl = curl_init($uri);
+        curl_setopt_array($curl, [
+            CURLOPT_FAILONERROR => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_MAXREDIRS => 5,
+            CURLOPT_FORBID_REUSE => true,
+            CURLOPT_FRESH_CONNECT => true,
+            CURLOPT_RETURNTRANSFER => true,
+        ]);
+        $result = @curl_exec($curl);
+        if($result != '') {
+            $result = @json_decode($result);
+        }
+        return $result && isset($result->query->results->channel) ? $result->query->results->channel : false;
+    }
+
+    static private function formatTemperature($temp, $unit) {
+        switch(strtoupper($unit)) {
+        case 'C':   return (string)(int)round((float)$temp) . '℃';
+        case 'F':   return (string)(int)round(self::f2c($temp)) . '℃';
+        default:    return '??℃';
+        }
+    }
+
+    static private function formatPressure($pressure, $unit) {
+        switch($unit) {
+        case 'in': return sprintf('%dhPa', round(self::inhg2hpa($pressure)));
+        default:   return '??hPa';
+        }
+    }
+
+    static private function formatWind($compass, $speed, $unit) {
+        switch($unit) {
+        default:    return '風向風速不明';
+        case 'mph': $speed = self::miph2mps($speed); break;
+        }
+        if($speed < 1) {
+            return 'ほぼ無風';
+        }
+        $compass16 = floor((11.25 + $compass) * 16 / 360) % 16;
+        $compass_map = [
+            '北', '北北東', '北東', '東北東', '東', '東南東', '南東', '南南東',
+            '南', '南南西', '南西', '西南西', '西', '西北西', '北西', '北北西',
+        ];
+        return sprintf('%sの風%dm', $compass_map[$compass16], round($speed));
+    }
+
+    static private function formatWeather($code, $fallback) {
+        $map = [
+             0  => '竜巻',
+             1  => '台風',
+             2  => 'ハリケーン',
+             3  => '激しい雷雨',
+             4  => '雷雨',
+             5  => '雪混じりの雨',
+             6  => 'みぞれ混じりの雨',
+             7  => 'みぞれ混じりの雪',
+             8  => '着氷性の霧雨',
+             9  => '霧雨',
+            10  => '着氷性の雨',
+            11  => 'にわか雨',
+            12  => 'にわか雨',
+            13  => '雪の突風',
+            14  => '時々雪',
+            15  => '吹雪',
+            16  => '雪',
+            17  => '雹',
+            18  => 'みぞれ',
+            19  => 'ほこり',
+            20  => '霧',
+            21  => '靄',
+            22  => '埃っぽい',
+            23  => '荒れ模様',
+            24  => '強風',
+            25  => '寒い',
+            26  => '曇り',
+            27  => 'おおむね曇り',
+            28  => 'おおむね曇り',
+            29  => 'ところにより曇り',
+            30  => 'ところにより曇り',
+            31  => '快晴',
+            32  => '晴れ',
+            33  => '晴れ',
+            34  => '晴れ',
+            35  => '雨と雹',
+            36  => '暑い',
+            37  => '局地的に雷雨',
+            38  => 'ところにより雷雨',
+            39  => 'ところにより雷雨',
+            40  => 'ところによりにわか雨',
+            41  => '大雪',
+            42  => '吹雪',
+            43  => '大雪',
+            44  => 'ところにより曇り',
+            45  => '雷雨',
+            46  => '吹雪',
+            47  => 'ところにより雷雨',
+        ];
+        if(isset($map[$code])) {
+            return $map[$code];
+        }
+        return sprintf('%s(%d)', $fallback, $code);
+    }
+
+    static private function f2c($f) {
+        return ((float)$f - 32) * 5 / 9;
+    }
+
+    static private function inhg2hpa($inhg) {
+        return 3386.39 * (float)$inhg / 100;
+    }
+
+    static private function miph2mps($mi) {
+        return 1609.344 * (float)$mi / 3600;
+    }
+}
---
+++ tarte/tartePlugins/yahoo_weather.plugin.php
@@ -0,0 +1,110 @@
+<?php
+require_once(__DIR__ . '/../components/YahooWeather.php');
+
+function plugin_yahoo_weather(TwStatus $status = null, DictionaryCandidate $candidate, array $params) {
+     if(!$status || !$status->user) {
+         throw new CException('precondition failed');
+     }
+ 
+    $locations = plugin_yahoo_weather__locations();
+    $location_regex = '/' . implode('|', array_map(function($a) { return preg_quote($a, '/'); }, array_keys($locations))) . '/u';
+
+    if(preg_match($location_regex, $status->parsed->text, $match)) {
+        $location = $match[0];
+    } else {
+        $keys = array_keys($locations);
+        $location = $keys[mt_rand(0, count($keys) - 1)];
+    }
+    $weather = new YahooWeather();
+    if(!$ret = $weather->get($locations[$location])) {
+        return '気象情報の取得に失敗しました';
+    }
+    return sprintf(
+        '%s現在の%sの天気は%s、気温%s、湿度%s、%s、気圧%s、明日の天気は%s、最低気温%s、最高気温%sです',
+        $ret['updated_at'] === false ? '' : date('H:i', $ret['updated_at']),
+        $location,
+        $ret['now']['weather'],
+        $ret['now']['temp'],
+        $ret['now']['humidity'],
+        $ret['now']['wind'],
+        $ret['now']['pressure'],
+        $ret['tomorrow']['weather'],
+        $ret['tomorrow']['temp_l'],
+        $ret['tomorrow']['temp_h']
+    );
+}
+
+function plugin_yahoo_weather__locations() {
+    return array_merge(
+        [
+            '北海道'    => 'hokkaido prefecture',
+            '青森'      => 'aomori prefecture',
+            '岩手'      => 'iwate prefecture',
+            '宮城'      => 'miyagi prefecture',
+            '秋田'      => 'akita prefecture',
+            '山形'      => 'yamagata prefecture',
+            '福島'      => 'fukushima prefecture',
+            '茨城'      => 'ibaraki prefecture',
+            '栃木'      => 'tochigi prefecture',
+            '群馬'      => 'gunma prefecture',
+            '埼玉'      => 'saitama prefecture',
+            '千葉'      => 'chiba prefecture',
+            '東京'      => 'tokyo prefecture',
+            '神奈川'    => 'kanagawa prefecture',
+            '新潟'      => 'niigata prefecture',
+            '富山'      => 'toyama prefecture',
+            '石川'      => 'ishikawa prefecture',
+            '福井'      => 'fukui prefecture',
+            '山梨'      => 'yamanashi prefecture',
+            '長野'      => 'nagano prefecture',
+            '岐阜'      => 'gifu prefecture',
+            '静岡'      => 'shizuoka prefecture',
+            '愛知'      => 'aichi prefecture',
+            '三重'      => 'mie prefecture',
+            '滋賀'      => 'shiga prefecture',
+            '京都'      => 'kyoto prefecture',
+            '大阪'      => 'osaka prefecture',
+            '兵庫'      => 'hyogo prefecture',
+            '奈良'      => 'nara prefecture',
+            '和歌山'    => 'wakayama prefecture',
+            '鳥取'      => 'tottori prefecture',
+            '島根'      => 'shimane prefecture',
+            '岡山'      => 'okayama prefecture',
+            '広島'      => 'hiroshima prefecture',
+            '山口'      => 'yamaguchi prefecture',
+            '徳島'      => 'tokushima prefecture',
+            '香川'      => 'kagawa prefecture',
+            '愛媛'      => 'ehime prefecture',
+            '高知'      => 'kochi prefecture',
+            '福岡'      => 'fukuoka prefecture',
+            '佐賀'      => 'saga prefecture',
+            '長崎'      => 'nagasaki prefecture',
+            '熊本'      => 'kumamoto prefecture',
+            '大分'      => 'oita prefecture',
+            '宮崎'      => 'miyazaki prefecture',
+            '鹿児島'    => 'kagoshima prefecture',
+            '沖縄'      => 'okinawa prefecture',
+        ], [
+            '札幌'      => 'sapporo-shi',
+            '仙台'      => 'sendai-shi',
+            'さいたま'  => 'saitama-shi',
+            '千葉'      => 'chiba-shi',
+            '横浜'      => 'yokohama-shi',
+            '川崎'      => 'kawasaki-shi',
+            '相模原'    => 'sagamihara-shi',
+            '新潟'      => 'niigata-shi',
+            '静岡'      => 'shizuoka-shi',
+            '浜松'      => 'hamamatsu-shi',
+            '名古屋'    => 'nagoya-shi',
+            '京都'      => 'kyoto-shi',
+            '大阪'      => 'osaka-shi',
+            '堺'        => 'sakai-shi',
+            '神戸'      => 'kobe-shi',
+            '岡山'      => 'okayama-shi',
+            '広島'      => 'hiroshima-shi',
+            '北九州'    => 'kitakyushu-shi',
+            '福岡'      => 'fukuoka-shi',
+            '熊本'      => 'kumamoto-shi',
+        ]
+    );
+}
